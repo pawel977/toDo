41e987f8808458a596d3bd7af32085766dd9d339
Paweł Sawicki <psawicki97@gmail.com>
2019-05-23 22:39:51 +0200

    INIT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToDoCardComponent } from './mainView/to-do-card/to-do-card.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoCardComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mainView/to-do-card/to-do-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-card',
+  templateUrl: './to-do-card.component.html',
+  styleUrls: ['./to-do-card.component.scss']
+})
+export class ToDoCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
